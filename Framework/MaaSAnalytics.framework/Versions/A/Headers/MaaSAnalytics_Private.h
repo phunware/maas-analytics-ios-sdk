de1a9222ab9086bb2f9b9186c69594a9d5c02bad
Illya Busigin <illyabusigin@Illyas-MacBook-Pro-2.local>
2013-06-24 14:31:23 -0500

    Adding MaaSAnalytics.framework

---
+++ Framework/MaaSAnalytics.framework/Versions/A/Headers/MaaSAnalytics_Private.h
@@ -0,0 +1,21 @@
+//
+//  MaaSAnalytics_Private.h
+//  MaaSAnalytics
+//
+//  Created by Illya Busigin on 6/21/13.
+//  Copyright (c) 2013 Phunware Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PWAnalyticEvent.h"
+
+
+@interface MaaSAnalytics ()
+
++ (MaaSAnalytics *)_sharedInstance;
+
+- (void)addEventToCache:(PWAnalyticEvent *)analyticsEvent;
+- (void)flushEventCacheIfNecessary;
+- (void)flushEventCache;
+
+@end
